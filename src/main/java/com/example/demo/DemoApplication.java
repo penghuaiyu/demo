fceaf6597c448a51b92c6b603d2579a9a52dd1ee
penghy1 <penghy1@lenovo.com>
2017-10-18 10:48:28 +0800

    修改一下类

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
 	@RequestMapping(value = "/")
 	public String home( ) {
-		return "a  Hello World!!!!";
+		return "Hello World!!!!";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
